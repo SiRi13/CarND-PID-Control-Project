306e498d3645209d8679d6467c0859a6ef98610f
Simon <simon.ritzel+git@gmail.com>
2017-08-14 18:24:50 +0200

    first commit

--- src/PID.cpp
+++ src/PID.cpp
@@ -1,9 +1,9 @@
-#include "PID.h"
+#include "PID.hpp"
 
 using namespace std;
 
 /*
-* TODO: Complete the PID class.
+* PID class.
 */
 
 PID::PID() {}
@@ -11,11 +11,15 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+  this->Kp = Kp;
+  this->Ki = Ki;
+  this->Kd = Kd;
 }
 
 void PID::UpdateError(double cte) {
+  d_error = cte - p_error;
+  p_error = cte;
+  i_error += cte;
 }
 
-double PID::TotalError() {
-}
-
+double PID::TotalError() { return -Kp * p_error - Kd * d_error - Ki * i_error; }
